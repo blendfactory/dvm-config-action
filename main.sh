990a4ce0bd524af73788a476ed58cb39708ac26a
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-30 23:50:35 +0900

    fix: Update RUNNER_OS conditionals in main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ "$RUNNER_OS" =~ ^windows* ]]; then
+if [[ "$RUNNER_OS" == Windows ]]; then
     executables/windows/app.exe $CONFIG_FILE_PATH
-elif [[ "$RUNNER_OS" =~ ^ubuntu* ]]; then
+elif [[ "$RUNNER_OS" == Linux ]]; then
     executables/linux/app.exe $CONFIG_FILE_PATH
-elif [[ "$RUNNER_OS" =~ ^macos* ]]; then
+elif [[ "$RUNNER_OS" == macOS ]]; then
     executables/macos/app.exe $CONFIG_FILE_PATH
 else
     echo "Unsupported OS: $RUNNER_OS";
