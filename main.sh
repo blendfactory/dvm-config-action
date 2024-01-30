cbffe6f49247bc3f513ea3abef0f4e1f9d7856c5
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-30 23:36:14 +0900

    fix: Add CONFIG_FILE_PATH environment variable to action.yaml and pass it to the app.exe command in main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ "$RUNNER_OS" =~ ^windows* ]]; then
-    executables/windows/app.exe
+    executables/windows/app.exe $CONFIG_FILE_PATH
 elif [[ "$RUNNER_OS" =~ ^ubuntu* ]]; then
-    executables/linux/app.exe
+    executables/linux/app.exe $CONFIG_FILE_PATH
 elif [[ "$RUNNER_OS" =~ ^macos* ]]; then
-    executables/macos/app.exe
+    executables/macos/app.exe $CONFIG_FILE_PATH
 else
     echo "Unsupported OS: $RUNNER_OS";
     exit 1;
